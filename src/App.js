037287d45c6b35048a89137df1996ad584c2f497
alaminarmanjanina <81838250+alaminarmanjanina@users.noreply.github.com>
2022-10-07 12:32:08 +0600

    bootstrap setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from "./logo.svg";
+import { Button, Spinner } from "react-bootstrap";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <h1>this is my first commit </h1>
+      <Button variant="success">my button </Button>
+      <Spinner animation="grow" variant="danger" />
     </div>
   );
 }
